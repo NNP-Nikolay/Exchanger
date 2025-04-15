a3d435fd6690d1d6009e08059a81d3927b924fdf
nnp <nnp.nikiforuk@gmail.com>
2025-04-15 03:36:21 +0300

    fix bags with ui

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Header: FC = () => {
   return (
@@ -14,8 +14,39 @@ const Header: FC = () => {
         padding: '32px 32px 0px 32px',
       }}
     >
-      <Box>
-        <img src="/logo.png" width={200} height={200} alt="logo" />
+      <Box component="div">
+        <img
+          style={{
+            borderRadius: '10px',
+            border: '1px solid white',
+            opacity: '0.6',
+            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.05)',
+          }}
+          src="/logo.jpg"
+          width={200}
+          height={200}
+          alt="logo"
+        />
+      </Box>
+
+      <Box
+        component="div"
+        sx={{
+          position: 'absolute',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            opacity: '0.8',
+            textShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)',
+            textTransform: 'uppercase',
+          }}
+        >
+          Rates may glitch, but we won’t!
+        </Typography>
       </Box>
     </Box>
   )
